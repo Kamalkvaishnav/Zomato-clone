8a1616d26f7aff0a418777acb42196ec6090634e
Mahendra Suthar <mahendra.suthar.1290@gmail.com>
2021-11-13 22:12:22 +0530

    remove unnecessary code

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,23 +1,8 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const Profile = () => {
-  const { user, isAuthenticated, isLoading, getAccessTokenSilently } = useAuth0();
-  useEffect(() => {
-    // let token;
-    // getAccessTokenSilently().then((tok) => {
-    //   token = tok;
-    //   console.log(token, "got");
-    //   fetch("http://localhost:5000/", {
-    //     headers: {
-    //       Authorization: `Bearer ${token}`,
-    //     },
-    //   })
-    //     .then((res) => res.json())
-    //     .then((res) => console.log(res))
-    //     .catch((err) => console.log(err));
-    // });
-  }, []);
+  const { user, isAuthenticated, isLoading } = useAuth0();
 
   if (isLoading) {
     return <div>Loading ...</div>;
@@ -30,6 +15,7 @@ const Profile = () => {
         <img src={user.picture} alt={user.name} />
         <h2>{user.name}</h2>
         <p>{user.email}</p>
+        <p>{user.sub}</p>
       </div>
     )
   );
