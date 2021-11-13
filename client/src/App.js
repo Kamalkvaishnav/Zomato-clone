7df2309381737c7accccfdbb406646ef53e66556
Mahendra Suthar <mahendra.suthar.1290@gmail.com>
2021-11-13 15:07:22 +0530

    integrate auth0 login/signup with email and google

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <nav>
+          <LoginButton />
+          <LogoutButton />
+          <Profile />
+        </nav>
         <p className="bg-blue-500 text-center p-6">hello world</p>
       </header>
     </div>
