32b616de100330f2f14f28fc93464397cc7cddb9
Mahendra Suthar <mahendra.suthar.1290@gmail.com>
2021-11-14 10:15:59 +0530

    add sign up button

--- client/src/components/header/profile.js
+++ client/src/components/header/profile.js
@@ -2,6 +2,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
 import LoginButton from "../LoginButton";
 import LogoutButton from "../LogoutButton";
+import SignupButton from "../SignupButton";
 
 function Profile() {
   const { user, isAuthenticated } = useAuth0();
@@ -10,14 +11,15 @@ function Profile() {
       {isAuthenticated ? (
         <>
           <div className="p-6 ml-80 flex">
-            <i class="fas fa-user mr-3 justify-center align-middle  text-xl"></i>
+            <i className="fas fa-user mr-3 justify-center align-middle  text-xl"></i>
             Hey {user.name}
           </div>
           <LogoutButton />
         </>
       ) : (
-        <div className="flex p-6 ml-72 ">
+        <div className="flex p-6 ml-72 space-x-4">
           <LoginButton />
+          <SignupButton />
         </div>
       )}
     </div>
